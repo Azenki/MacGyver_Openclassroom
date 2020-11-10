c4e4c031d423598b9d65e6b1070248b8b31e3b6f
Azenki <azncharles@zone-dxd.fr>
2020-11-11 00:07:19 +0100

    [ADD] print and move player as "S"

--- graphic/curses.py
+++ graphic/curses.py
@@ -8,3 +8,35 @@ class Window:
 
     def end(self):
         curses.endwin()
+    
+    def move_bottom(self, pos, source):
+        index = pos.x + pos.y * 16
+        tmp = list(source)
+        tmp[index] = '*'
+        tmp[index + 16] = 'S'
+        new = ''.join(tmp)
+        return (new)
+    
+    def move_top(self, pos, source):
+        index = pos.x + pos.y * 16
+        tmp = list(source)
+        tmp[index] = '*'
+        tmp[index - 16] = 'S'
+        new = ''.join(tmp)
+        return (new)
+
+    def move_right(self, pos, source):
+        index = pos.x + pos.y * 16
+        tmp = list(source)
+        tmp[index] = '*'
+        tmp[index + 1] = 'S'
+        new = ''.join(tmp)
+        return (new)
+
+    def move_left(self, pos, source):
+        index = pos.x + pos.y * 16
+        tmp = list(source)
+        tmp[index] = '*'
+        tmp[index - 1] = 'S'
+        new = ''.join(tmp)
+        return (new)
